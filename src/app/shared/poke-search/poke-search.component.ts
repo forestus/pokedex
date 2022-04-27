c8fff71876f5aa346a2190b4a4579cab428ee333
Guilherme Pereira Maciel <guilhermepm164@gmail.com>
2022-04-26 22:06:59 -0300

    add poke search

---
+++ src/app/shared/poke-search/poke-search.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'poke-search',
+  templateUrl: './poke-search.component.html',
+  styleUrls: ['./poke-search.component.scss']
+})
+export class PokeSearchComponent implements OnInit {
+@Output() public emitSearch: EventEmitter<string> = new EventEmitter()
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  public search(textInput: string){
+    this.emitSearch.emit(textInput)
+  }
+}
